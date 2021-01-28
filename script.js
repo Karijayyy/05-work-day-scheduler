78c5ff0b3f10db13e859f4f733f84d7f780006b4
Kari Johnson <kari@crownmediamarketing.com>
2021-01-28 14:03:06 -0700

    initial commit

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-// create calendar with query selector 
-// create for loop for the hours of the day
+// using moment.js
+// create variables
+let today = moment().format("MMM Do YY");
+let dayOfWeek = moment().format('dddd');
+let currentHour = moment().hour(); 
+let ToDoListArray [];
+
+// create functions 
