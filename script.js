6b2c09772fcbd36ad54f2748670046590bd66d2d
Kari Johnson <kari@crownmediamarketing.com>
2021-01-29 07:37:45 -0700

    fixed some typos

--- script.js
+++ script.js
@@ -1,58 +1,58 @@
 // using moment.js
 // create variables
-let today = moment().format("MMM DD YY");
+let today = moment().format("MM DD YY");
 let dayOfWeek = moment().format('dddd');
 let currentHour = moment().hour();
 let ToDoListArray = [];
 
 // Dom event
-$("#currentDay").text(dayOfWeek + '' + today);
+$("#currentDay").text(dayOfWeek + ' ' + today);
 
-// create functions 
-for (i = 9; i < 17; i++) {
+// create functions changing colors 
+for (i = 9; i < 18; i++) {
     if (parseInt($('#' + i).attr('id')) < currentHour) {
         $('#' + i).attr('style', 'background-color: #F5F5F5');
     } else if (parseInt($('#' + i).attr('id')) > currentHour) {
-        $('#' + i).attr('style', 'background-color: #CD5C5C');
+        $('#' + i).attr('style', 'background-color: #F5F5DC');
     } else {
-        $('#' + i).attr('style', 'background-color: #8B0000')
+        $('#' + i).attr('style', 'background-color: #FFE4E1')
     }
-
-    $('.save').on('click', function() {
-        toDoListArray = [];
-    
-        for (i = 9; i < 1; i++) {
-            let toDoListValue = $('#' + i).val();
-    
-            let toDoListObject = {
-                toDoListHour: i,
-                toDoListItem: toDoListValue
-            }
-    
-            toDoListArray.push(toDoListObject);
-    
-            localStorage.setItem("toDoList", JSON.stringify(toDoListArray));
-        }
-    })
-    
-    $('#clear-button').on('click', function() {
-        localStorage.clear();
-        $('textarea').val('');
-    })
-    
-    function loadToDo() {
-        let storedTodo = JSON.parse(localStorage.getItem("toDos"));
-        console.log(storedTodo);
-    
-        if (storedTodo != null) {
-            for (i = 0; i < storedTodo.length; i++) {
-                $('#' + storedTodo[i].todoHour).val(storedTodo[i].todoItem);
-            };
-        }
-    }
-    
 }
-    loadToDoList();
+// saves array 
+$('.save').on('click', function () {
+    toDoListArray = [];
 
-// create even listeners
+    for (i = 9; i < 18; i++) {
+        let todoValue = $('#' + i).val();
+
+        let todoObject = {
+            todoHour: i,
+            todoItem: todoValue
+        }
+
+        toDoListArray.push(todoObject);
+
+        localStorage.setItem("todo", JSON.stringify(toDoListArray));
+    }
+})
+// clear out local storage
+$('#clear-button').on('click', function () {
+    localStorage.clear();
+    $('textarea').val('');
+})
+//show to do list
+function loadTodo() {
+    let storedTodo = JSON.parse(localStorage.getItem("todo"));
+    console.log(storedTodo);
+
+    if (storedTodo !== null) {
+        for (i = 0; i < storedTodo.length; i++) {
+            $('#' + storedTodo[i].todoHour).val(storedTodo[i].todoItem);
+        };
+    }
+}
+loadToDoList();
+    
+
+
 
